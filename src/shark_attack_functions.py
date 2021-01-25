3e1cb528002b718128346518c061fc162ad97490
Alfredo Zavala <alfredod.mba2021@london.edu>
2021-01-25 18:30:46 +0100

    functions in different file

---
+++ src/shark_attack_functions.py
@@ -0,0 +1,11 @@
+def convert_float_to_int(flt):
+    try:
+        return int(flt)
+    except:
+        return 0
+
+def convert_str_to_int(string):
+    try:
+        return int(string)
+    except:
+        return 0
